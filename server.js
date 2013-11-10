7d551dd921b613bd39f996f82c743a4fa239b667
Brian Green <briangreenery@gmail.com>
2013-11-10 15:50:29 -0800

    Ajax posting & redirect in almost certainly the wrong way

--- server.js
+++ server.js
@@ -9,8 +9,12 @@ app.set('view engine', 'hbs');
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', function (req, res) {
+app.get('/gist', function (req, res) {
   res.render('home');
+});
+
+app.post('/gist/create', function (req, res) {
+  res.send('123');
 });
 
 app.listen(3000);
