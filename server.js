3b6b7c8f0e0ed99d40d613a92a998187933fed4d
Brian Green <briangreenery@gmail.com>
2013-11-10 16:41:48 -0800

    Minimal demo

--- server.js
+++ server.js
@@ -1,20 +1,47 @@
 var express = require('express');
 var exphbs = require('express3-handlebars');
 var path = require('path');
+var fs = require('fs');
 
 var app = express();
+
+var randomName = function (length) {
+  var alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+
+  var name = '';
+  for (var i = 0; i < length; i++) {
+    var number = Math.floor(Math.random() * alphabet.length);
+    name += alphabet[number];
+  }
+
+  return name;
+}
+
+var uploadsDir = path.join(__dirname, 'uploads');
+
+if (!fs.existsSync(uploadsDir))
+  fs.mkdirSync(uploadsDir);
 
 app.engine('hbs', exphbs({defaultLayout: 'main.hbs'}));
 app.set('view engine', 'hbs');
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use('/gist', express.static(path.join(__dirname, 'public')));
 
 app.get('/gist', function (req, res) {
   res.render('home');
 });
 
+app.get('/gist/:id', function (req, res) {
+  var gist = JSON.parse(fs.readFileSync(path.join(uploadsDir, req.params.id)));
+  res.render('gist', gist);
+});
+
 app.post('/gist/create', function (req, res) {
-  res.send('123');
+  var name = randomName(9);
+  req.pipe(fs.createWriteStream(path.join(uploadsDir, name)));
+  req.on('end', function () {
+    res.send(name);
+  });
 });
 
 app.listen(3000);
