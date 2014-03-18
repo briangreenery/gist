3811961255dd98f489ee0292d99dad54ce9a410a
Brian Green <briangreenery@gmail.com>
2014-03-18 13:53:34 -0700

    Add coffeescript too

--- languages.js
+++ languages.js
@@ -1,14 +1,15 @@
 module.exports = [
-  {name:'C++',        mode:'c_cpp'},
-  {name:'CSS',        mode:'css'},
-  {name:'HTML',       mode:'html'},
-  {name:'JavaScript', mode:'javascript'},
-  {name:'JSON',       mode:'json'},
-  {name:'Perl',       mode:'perl'},
-  {name:'Python',     mode:'python'},
-  {name:'Ruby',       mode:'ruby'},
-  {name:'Shell',      mode:'sh'},
-  {name:'SQL',        mode:'sql'},
-  {name:'Text',       mode:'text'},
-  {name:'XML',        mode:'xml'}
+  {name:'C++',          mode:'c_cpp'},
+  {name:'CoffeeScript', mode:'coffee'},
+  {name:'CSS',          mode:'css'},
+  {name:'HTML',         mode:'html'},
+  {name:'JavaScript',   mode:'javascript'},
+  {name:'JSON',         mode:'json'},
+  {name:'Perl',         mode:'perl'},
+  {name:'Python',       mode:'python'},
+  {name:'Ruby',         mode:'ruby'},
+  {name:'Shell',        mode:'sh'},
+  {name:'SQL',          mode:'sql'},
+  {name:'Text',         mode:'text'},
+  {name:'XML',          mode:'xml'}
 ];
